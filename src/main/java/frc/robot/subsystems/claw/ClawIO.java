48ce99cb1c66233007f44ca0a6c741630916ef51
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-06-27 11:37:47 +0800

    Claw subsystem

---
+++ src/main/java/frc/robot/subsystems/claw/ClawIO.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems.claw;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ClawIO {
+  @AutoLog
+  public static class ClawIOInputs {
+    public double IntakePositionRad = 0.0;
+    public double IntakeVelocityRadPerSec = 0.0;
+    public double IntakeAppliedVolts = 0.0;
+    public double IntakeCurrentAmps = 0.0;
+    public double ShooterPositionRad = 0.0;
+    public double ShooterVelocityRadPerSec = 0.0;
+    public double ShooterAppliedVolts = 0.0;
+    public double ShooterCurrentAmps = 0.0;
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(ClawIOInputs inputs) {}
+
+  /** Run the roller at the specified open loop value. */
+  public default void setOpenLoop(double intakeOutput, double shooterOutput) {}
+}
