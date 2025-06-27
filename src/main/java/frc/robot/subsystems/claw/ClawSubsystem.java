297130cebc5548666eafe3d45f82afc03f0c8d46
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-06-27 15:53:54 +0800

    Change to wpi sysid

--- src/main/java/frc/robot/subsystems/claw/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/claw/ClawSubsystem.java
@@ -2,27 +2,29 @@
 
 package frc.robot.subsystems.claw;
 
+import static edu.wpi.first.units.Units.Volt;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import org.littletonrobotics.junction.Logger;
 
 public class ClawSubsystem extends SubsystemBase {
-  public enum clawIdleState {
-    in,
-    out,
-    stop
-  }
-
-  public enum climberIdleState {
-    in,
-    out,
-    stop
-  }
-
   private final ClawIO io;
   private final ClawIOInputsAutoLogged inputs = new ClawIOInputsAutoLogged();
+  private final SysIdRoutine sysId;
 
   public ClawSubsystem(ClawIO io) {
     this.io = io;
+    sysId =
+        new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                (state) -> Logger.recordOutput("Claw/Intake/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism(
+                (voltage) -> setIntakeVoltage(voltage.in(Volt)), null, this));
   }
 
   public void periodic() {
@@ -48,5 +50,13 @@ public class ClawSubsystem extends SubsystemBase {
 
   public void setShooterVoltage(double volts) {
     io.setShooterVoltage(volts);
+  }
+
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return sysId.quasistatic(direction);
+  }
+
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return sysId.dynamic(direction);
   }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -2,20 +2,18 @@
 
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Voltage;
+import static edu.wpi.first.units.Units.Volt;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-
-import static edu.wpi.first.units.Units.Volt;
-
 import org.littletonrobotics.junction.Logger;
 
 public class IntakeSubsystem extends SubsystemBase {
   private IntakeIO io;
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
   private final SysIdRoutine sysId;
+
   public IntakeSubsystem(IntakeIO io) {
     this.io = io;
     sysId =
@@ -24,9 +22,8 @@ public class IntakeSubsystem extends SubsystemBase {
                 null,
                 null,
                 null,
-                (state) -> Logger.recordOutput("Arm/SysIdState", state.toString())),
-            new SysIdRoutine.Mechanism(
-              (voltage) -> setArmVoltage(voltage.in(Volt)), null, this));
+                (state) -> Logger.recordOutput("Intake/Arm/SysIdState", state.toString())),
+            new SysIdRoutine.Mechanism((voltage) -> setArmVoltage(voltage.in(Volt)), null, this));
   }
 
   public void periodic() {
@@ -42,10 +39,11 @@ public class IntakeSubsystem extends SubsystemBase {
   public void runVelocity(double indexerOutput, double intakeOutput, double armOutput) {
     io.setOpenLoop(indexerOutput, intakeOutput, armOutput);
   }
-  
-  public void setArmVoltage(double volts){
+
+  public void setArmVoltage(double volts) {
     io.setArmVoltage(volts);
   }
+
   public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
     return sysId.quasistatic(direction);
   }
