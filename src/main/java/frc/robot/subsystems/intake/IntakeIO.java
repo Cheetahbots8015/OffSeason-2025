19477a9582e40ba16dc05292d4f00a0e824da6e7
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-06-28 19:51:42 +0800

    Delete useless semicolon

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -29,11 +29,13 @@ public interface IntakeIO {
   /** Run the roller at the specified open loop value. */
   public default void setOpenLoop(double indexerOutput, double intakeOutput, double armOutput) {}
 
-  public default void setArmVoltage(double volts){}
+  public default void setArmVoltage(double volts) {}
 
-  public default void setIntakeVoltage(double volts){}
+  public default void setIntakeVoltage(double volts) {}
 
-  public default void setIndexerVoltage(double volts){}
+  public default void setIndexerVoltage(double volts) {}
 
-  public default boolean getCanRange(){return false;}
+  public default boolean getCanRange() {
+    return false;
+  }
 }
