2951f0088af8819743542563aa6109769c79abbb
michaelmjhhhh <mjh4311@163.com>
2025-06-28 18:47:08 +0800

    Seperating pivot and claw command, put them in one folder

--- src/main/java/frc/robot/commands/ClimberCommand/ClimbePivotDownCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommand/ClimbePivotDownCommand.java
@@ -1,12 +1,12 @@
-package frc.robot.commands;
+package frc.robot.commands.ClimberCommand;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 
-public class ClimberInCommand extends Command {
+public class ClimbePivotDownCommand extends Command {
   private final ClimberSubsystem m_subsystem;
 
-  public ClimberInCommand(ClimberSubsystem subsystem) {
+  public ClimbePivotDownCommand(ClimberSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -19,8 +19,7 @@ public class ClimberInCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.runDutyCyleOutValue(
-        m_subsystem.pivotUpDutyCycleOutValue, m_subsystem.clawDutyCycleOutValue);
+    m_subsystem.runDutyCyleOutValue(m_subsystem.pivotDownDutyCycleOutValue, 0.0);
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/ClimberCommand/ClimberPivotUpCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommand/ClimberPivotUpCommand.java
@@ -1,12 +1,12 @@
-package frc.robot.commands;
+package frc.robot.commands.ClimberCommand;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 
-public class ClimberOutCommand extends Command {
+public class ClimberPivotUpCommand extends Command {
   private final ClimberSubsystem m_subsystem;
 
-  public ClimberOutCommand(ClimberSubsystem subsystem) {
+  public ClimberPivotUpCommand(ClimberSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -19,7 +19,8 @@ public class ClimberOutCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.runDutyCyleOutValue(m_subsystem.pivotDownDutyCycleOutValue, 0.0);
+    m_subsystem.runDutyCyleOutValue(
+        m_subsystem.pivotUpDutyCycleOutValue,0.0);
   }
 
   // Called once the command ends or is interrupted.
