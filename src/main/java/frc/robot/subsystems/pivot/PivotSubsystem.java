fd10c319a04c92c58fb63ed3661711c2ade94683
JasonG25 <guozichen_jason@qq.com>
2025-06-29 16:42:07 +0800

    added IOInput getter

--- src/main/java/frc/robot/subsystems/pivot/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotSubsystem.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.constants.PivotConstants;
+import frc.robot.subsystems.pivot.PivotIO.PivotIOInputs;
 import org.littletonrobotics.junction.Logger;
 
 public class PivotSubsystem extends SubsystemBase {
@@ -74,5 +75,9 @@ public class PivotSubsystem extends SubsystemBase {
 
   public Command sysIdDynamic(SysIdRoutine.Direction direction) {
     return sysId.dynamic(direction);
+  }
+
+  public PivotIOInputs getInput() {
+    return inputs;
   }
 }
