b11bf8ab34e81cbfefc3610f9d7f980304769e42
JoeyZhuoer <zhaozhuoer2021@hotmail.com>
2025-07-10 12:10:53 +0800

    Fix merge

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -235,6 +235,7 @@ public class RobotContainer {
             new ElevatorSetPositionCommand(elevatorSubsystem, 1.615)
                 .andThen(new PivotSetPositionCommand(pivotSubsystem, 21.5))
                 .andThen(new ClawAlageShootCommand(clawSubsystem)));
+    */
 
     testController
         .povLeft()
